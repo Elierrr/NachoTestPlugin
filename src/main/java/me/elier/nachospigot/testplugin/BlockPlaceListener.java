809ee848ca4e7c7b676b6b3d22b66196c7e5e384
Elier Lopez <elierrrlg@outlook.com>
2021-12-05 12:15:12 -0500

    Log

--- src/main/java/me/elier/nachospigot/testplugin/BlockPlaceListener.java
+++ src/main/java/me/elier/nachospigot/testplugin/BlockPlaceListener.java
@@ -4,9 +4,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import java.util.logging.Level;
+
 public class BlockPlaceListener implements Listener {
     @EventHandler
     public void cancelEvent(BlockPlaceEvent e) {
         if(TestPlugin.getInstance().config.getBoolean("cancel-block-place-events")) e.setCancelled(true);
+        TestPlugin.getInstance().getLogger().log(Level.INFO, "Cancelled block place event.");
     }
 }
